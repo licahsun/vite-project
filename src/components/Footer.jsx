43e6d093c1dc672bac4b293ab106aae54ccd2657
licahsun <hsunclan@gmail.com>
2025-08-14 11:32:12 +0800

    0814

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>版權宣告</div>
+    )
+}
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import App from './App08005-1.jsx'
-import App from "./pages/App0813-weather3.jsx"
-import './assets/all.scss'
+// import App from './pages/Home'
+import App from './MyApp'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // 嚴謹模式
   <StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </StrictMode>,
 )
